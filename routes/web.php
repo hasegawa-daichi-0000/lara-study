4e9c40f024d66f27952f0a9c4d6fd2033c0cc88b
長谷川大地 <hasegawadaichi@hasegawadaichinoMacBook-Pro-2.local>
2023-10-19 11:06:23 +0900

    ECサイトの実装

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LineItemController;
+use App\Http\Controllers\CartController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// この書き方は8.8から追加されたメソッド
+Route::controller(ProductController::class)->group(function() {
+    Route::name('product.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/product/{id}', 'show')->name('show');
+    });
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(LineItemController::class)->group(function() {
+    Route::name('line_item.')->group(function() {
+        Route::post('/line_item/create', 'create')->name('create');
+        Route::post('/line_item/delete', 'delete')->name('delete');
+    });
+});
+
+Route::controller(CartController::class)->group(function() {
+    Route::name('cart.')->group(function () {
+        Route::get('/cart', 'index')->name('index');
+        Route::get('/cart/checkout', 'checkout')->name('checkout');
+        Route::get('/cart/success', 'success')->name('success');
+    });
 });
